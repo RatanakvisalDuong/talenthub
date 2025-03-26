082ed4438f16204ab74e98a43fb5a0e23eb2ac7b
RVisalD <126460312+RVisalD@users.noreply.github.com>
2025-03-26 18:32:29 +0700

    fix for login

--- src/dummydata/portfolio.ts
+++ src/dummydata/portfolio.ts
@@ -1,96 +1,108 @@
 export const portfolio = [
+        // {
+        //     'id': 1,
+        //     'name': 'Ratanakvisal Duong',
+        //     'phone_number': '017 6146 694',
+        //     'major': 1,
+        //     'working_status': 1,
+        //     'role': 1,
+
+        // },
+        // {
+        //     'id': 2,
+        //     'name': 'Lysa Sorkeo',
+        //     'phone_number': '017 6146 694',
+        //     'major': 1,
+        //     'working_status': 1,
+        //     'role': 1,
+        // },
+        // {
+        //     'id': 3,
+        //     'name': 'Chanphearun Cheam',
+        //     'phone_number': '017 6146 694',
+        //     'major': 2,
+        //     'working_status': 2,
+        //     'role': 1,
+
+        // },
+        // {
+        //     'id': 4,
+        //     'name': 'Thavorak Suon',
+        //     'phone_number': '017 6146 694',
+        //     'major': 1,
+        //     'working_status': 1,
+        //     'role': 1,
+
+        // },
+        // {
+        //     'id': 5,
+        //     'name': 'Kimsongyean Thy',
+        //     'phone_number': '017 6146 694',
+        //     'major': 2,
+        //     'working_status': 2,
+        //     'role': 1
+
+        // },
+        // {
+        //     'id': 6,
+        //     'name': 'Molika Keo',
+        //     'phone_number': '017 6146 694',
+        //     'major': 2,
+        //     'working_status': 2,
+        //     'role': 1
+
+        // },
+        // {
+        //     'id': 7,
+        //     'name': 'Panha Huot',
+        //     'phone_number': '017 6146 694',
+        //     'major': 1,
+        //     'working_status': 2,
+        //     'role': 1
+        // },
+        // {
+        //     'id': 8,
+        //     'name': 'Kimsinh Seang',
+        //     'phone_number': '017 6146 694',
+        //     'major': 2,
+        //     'working_status': 1,
+        //     'role': 1
+        // },
+        // {
+        //     'id': 9,
+        //     'name': 'Ratana Soth',
+        //     'phone_number': '017 6146 694',
+        //     'major': null,
+        //     'working_status': null,
+        //     'role': 2
+        // },
+        // {
+        //     'id': 10,
+        //     'name': 'Rathanak Sreang',
+        //     'phone_number': '017 6146 694',
+        //     'major': null,
+        //     'working_status': null,
+        //     'role': 2
+        // },
+        // {
+        //     'id': 11,
+        //     'name': 'Flordeliza Poncio',
+        //     'phone_number': '017 6146 694',
+        //     'major': null,
+        //     'working_status': null,
+        //     'role': 2
+        // },
         {
             'id': 1,
-            'name': 'Ratanakvisal Duong',
-            'phone_number': '017 6146 694',
-            'major': 1,
-            'working_status': 1,
-            'role': 1,
-
-        },
-        {
-            'id': 2,
-            'name': 'Lysa Sorkeo',
-            'phone_number': '017 6146 694',
-            'major': 1,
-            'working_status': 1,
-            'role': 1,
-        },
-        {
-            'id': 3,
-            'name': 'Chanphearun Cheam',
-            'phone_number': '017 6146 694',
-            'major': 2,
-            'working_status': 2,
-            'role': 1,
-
-        },
-        {
-            'id': 4,
-            'name': 'Thavorak Suon',
-            'phone_number': '017 6146 694',
-            'major': 1,
-            'working_status': 1,
-            'role': 1,
-
-        },
-        {
-            'id': 5,
-            'name': 'Kimsongyean Thy',
-            'phone_number': '017 6146 694',
-            'major': 2,
-            'working_status': 2,
-            'role': 1
-
-        },
-        {
-            'id': 6,
-            'name': 'Molika Keo',
-            'phone_number': '017 6146 694',
-            'major': 2,
-            'working_status': 2,
-            'role': 1
-
-        },
-        {
-            'id': 7,
-            'name': 'Panha Huot',
-            'phone_number': '017 6146 694',
-            'major': 1,
-            'working_status': 2,
-            'role': 1
-        },
-        {
-            'id': 8,
-            'name': 'Kimsinh Seang',
-            'phone_number': '017 6146 694',
-            'major': 2,
-            'working_status': 1,
-            'role': 1
-        },
-        {
-            'id': 9,
-            'name': 'Ratana Soth',
-            'phone_number': '017 6146 694',
-            'major': null,
-            'working_status': null,
-            'role': 2
-        },
-        {
-            'id': 10,
-            'name': 'Rathanak Sreang',
-            'phone_number': '017 6146 694',
-            'major': null,
-            'working_status': null,
-            'role': 2
-        },
-        {
-            'id': 11,
-            'name': 'Flordeliza Poncio',
-            'phone_number': '017 6146 694',
-            'major': null,
-            'working_status': null,
-            'role': 2
-        },
+        'user_id': 1,
+        'major': 1,
+        'phone_number': 'string',
+        'about': 'string',
+        'working_status': 1,
+        'status': 1,
+        'created_at': 'string',
+        'updated_at': 'string',
+        'name': 'string',
+        'role': 1,}
 
 ]
